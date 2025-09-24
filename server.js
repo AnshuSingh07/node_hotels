d91815b9027c7bcd050e6e60602d6c89c639f7c6
Anshu Singh <anshusinghofficial16@gmail.com>
2025-09-25 00:03:39 +0530

    added require for db js file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const db = require("./db");
 require("dotenv").config();
 
 app.use(bodyParser.json());
