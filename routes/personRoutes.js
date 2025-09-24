f38490dc07faedbca2968122d3b44a293825a87d
Anshu Singh <anshusinghofficial16@gmail.com>
2025-09-25 02:14:18 +0530

    added dynamic error in res of person get

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -28,6 +28,7 @@ router.get("/", async (req, res) => {
     res.status(200).json(data);
   } catch (error) {
     console.log(error);
+    res.status(statusCode).json({ error: error });
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
